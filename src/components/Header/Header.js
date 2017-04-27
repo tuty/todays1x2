019ffb76ea1ba0d483079e3589bdff3519df8436
Ivan Chervenkov <Ivan_Chervenkov@epam.com>
2017-04-27 17:17:29 +0300

    add avatar to nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import AppBar from 'material-ui/AppBar';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import SocialPerson from 'material-ui/svg-icons/social/person';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import { fullWhite } from 'material-ui/styles/colors';
+import ActionReorder from 'material-ui/svg-icons/action/reorder';
+import Avatar from 'material-ui/Avatar';
+import { Tabs, Tab } from 'material-ui/Tabs';
 
 import HeaderDrawer from './HeaderDrawer';
 
 
-class componentName extends Component {
+class Header extends Component {
 
     constructor(props, context) {
         super(props, context);
@@ -23,23 +33,76 @@ class componentName extends Component {
 
     render() {
 
-        const {title} = this.props;
-        const {openDrawer} = this.state;
+        const { title } = this.props;
+        const { openDrawer } = this.state;
 
         return (
-            <div>
-                <AppBar
-                    title={title}
-                    onLeftIconButtonTouchTap={this.handleOpenDrawer}
-                />
-                <HeaderDrawer title={title} openDrawer={openDrawer} handleCloseDrawer={this.handleCloseDrawer}/>
+            <div className={'app-header-container'}>
+                <Toolbar
+                    className={'app-header'}
+                    style={{
+                        "background-color": "#283E4A",
+                        "max-width": "1128px",
+                        "margin": "auto"
+                    }}
+                >
+                    <ToolbarGroup firstChild={true}>
+                        <IconButton
+                            onTouchTap={this.handleOpenDrawer}
+                        >
+                            <ActionReorder color={fullWhite} />
+                        </IconButton>
+                        <ToolbarTitle
+                            text={title}
+                            style={{
+                                color: "#FFF"
+                            }}
+                        />
+                    </ToolbarGroup>
+                    <ToolbarGroup lastChild={true}>
+                        <ToolbarSeparator
+                            style={{
+                                'background-color': '#FFF',
+                                margin: '0 1em',
+                                height: '90%'
+                            }}
+                        />
+                        <IconMenu
+                            iconButtonElement={
+                                <div>
+                                    <div
+                                        className={'avatar-container'}
+                                    >
+                                        <Avatar
+                                            size={24}
+                                            icon={<SocialPerson />}
+                                        />
+                                    </div>
+
+                                    <div
+                                        className={'label'}
+                                    >
+                                        Me
+                                        <i className="material-icons">&#xE5CF;</i>
+                                    </div>
+                                </div>
+
+                            }
+                        >
+                            <MenuItem primaryText="Download" />
+                            <MenuItem primaryText="More Info" />
+                        </IconMenu>
+                    </ToolbarGroup>
+                </Toolbar>
+
+                <HeaderDrawer title={title} openDrawer={openDrawer} handleCloseDrawer={this.handleCloseDrawer} />
             </div>
         );
     }
 }
 
-componentName.propTypes = {
+Header.propTypes = {
     title: PropTypes.string,
 };
 
-export default componentName;
+export default Header;
--- src/components/Header/HeaderDrawer.js
+++ src/components/Header/HeaderDrawer.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, IndexLink } from 'react-router';
 
-
-import AppBar from 'material-ui/AppBar';
+import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
 import Drawer from 'material-ui/Drawer';
+import {fullWhite} from 'material-ui/styles/colors';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 
-const HeaderDrawer = ({title, handleCloseDrawer, openDrawer}) => {
+const HeaderDrawer = ({ title, handleCloseDrawer, openDrawer }) => {
     return (
         <Drawer
             width={250}
@@ -18,11 +18,25 @@ const HeaderDrawer = ({title, handleCloseDrawer, openDrawer}) => {
             docked={false}
             onRequestChange={handleCloseDrawer}
         >
-            <AppBar
-                title={title}
-                iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-                onLeftIconButtonTouchTap={handleCloseDrawer}
-            />
+            <Toolbar
+                className={'app__drawer-header'}
+                style={{
+                    "background-color": "#283E4A"
+                }}
+            >
+                <ToolbarGroup
+                    firstChild={true}
+                    onTouchTap={handleCloseDrawer}
+                >
+                    <IconButton><NavigationClose color = {fullWhite}/></IconButton>
+                    <ToolbarTitle
+                        text={title}
+                        style={{
+                            color: "#FFF"
+                        }}
+                    />
+                </ToolbarGroup>
+            </Toolbar>
             <Menu>
                 <MenuItem
                     onTouchTap={handleCloseDrawer}
